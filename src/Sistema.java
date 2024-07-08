5c827875adfd1fd9f4d712228296520a90dfd0e8
gonzaga95 <gonzagas.dudu@gmail.com>
2024-07-08 19:52:43 -0400

    add controle da conta

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Sistema {
     public static void main(String[] args) {
 
@@ -9,5 +11,41 @@ public class Sistema {
         System.out.println("\nCliente: "+nomeCliente);
         System.out.println("Saldo atual: "+saldo);
         System.out.println("####################################");
+
+        int opcao = 0;
+        Scanner Scanner = new Scanner(System.in);
+
+        while (opcao != 9) {
+            System.out.println("""
+                    Selecione uma opção:
+                    
+                    [1] Consultar saldo
+                    [2] Depositar valor
+                    [3] Retirar valor
+                    [9] Sair
+                    """);
+            int opcaoSelecionada = Scanner.nextInt();
+            switch (opcaoSelecionada) {
+                case 1:
+                    System.out.println("Seu saldo é de: R$ " + saldo);
+                    break;
+                case 2:
+                    System.out.println("Insira o valor a depositar: ");
+                    double valorDeposito = Scanner.nextDouble();
+                    saldo += valorDeposito;
+                    break;
+                case 3:
+                    System.out.println("Insira o valor a retirar: ");
+                    double valorRetirada = Scanner.nextDouble();
+                    if (valorRetirada > saldo) {
+                        System.out.println("Saldo insuficiente");
+                    } else {
+                        saldo -= valorRetirada;
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException("Opção inválida: " + opcaoSelecionada);
+            }
+        }
     }
 }
