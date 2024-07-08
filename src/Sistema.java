092088959f77dbc65d02276d7c20b391627e80b9
gonzaga95 <gonzagas.dudu@gmail.com>
2024-07-08 19:37:24 -0400

    init class sistema, welcome client

---
+++ src/Sistema.java
@@ -0,0 +1,13 @@
+public class Sistema {
+    public static void main(String[] args) {
+
+        String nomeCliente = "Gato Felix";
+        double saldo = 200.00;
+
+        System.out.println("####################################");
+        System.out.println("Bem-vinde ao Banco Interativo Java");
+        System.out.println("\nCliente: "+nomeCliente);
+        System.out.println("Saldo atual: "+saldo);
+        System.out.println("####################################");
+    }
+}
